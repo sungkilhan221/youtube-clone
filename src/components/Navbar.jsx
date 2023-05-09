c795c810a4c859b50565e02a38f8f2eee1db7aab
Sung Kil Han <90734610+greatandrewhan@users.noreply.github.com>
2023-05-08 22:54:28 -0400

    navbar frame fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { Stack } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-import { logo } from '../utils/constants';
-import SearchBar from './SearchBar';
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
   <Stack
@@ -10,14 +10,15 @@ const Navbar = () => (
     alignItems="center"
     p={2}
     sx={{
-      position: 'sticky',
-      background: '#000',
+      position: "sticky",
+      background: "#000",
       top: 0,
-      justifyContent: 'space-between',
+      justifyContent: "space-between",
     }}
   >
-    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={logo} alt="logo" height={45} />
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />{" "}
+      <span className="logoText">YouTube</span>
     </Link>
     <SearchBar />
   </Stack>
